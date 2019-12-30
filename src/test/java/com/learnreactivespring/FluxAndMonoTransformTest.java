f56de28c6a9fc52d9a9a7759977b5df609d1a1fe
E050AQ <mlesniak@genuitek.com>
2019-12-30 14:07:07 +0100

    Uzywanie flatMap, symulator bazy danych + operacja parallel

--- src/test/java/com/learnreactivespring/FluxAndMonoTransformTest.java
+++ src/test/java/com/learnreactivespring/FluxAndMonoTransformTest.java
@@ -7,6 +7,8 @@ import reactor.test.StepVerifier;
 import java.util.Arrays;
 import java.util.List;
 
+import static reactor.core.scheduler.Schedulers.parallel;
+
 public class FluxAndMonoTransformTest {
 
     List<String> names = Arrays.asList("adam", "anna", "jack", "jenny");
@@ -57,5 +59,60 @@ public class FluxAndMonoTransformTest {
         StepVerifier.create(namesFlux)
                 .expectNext("JENNY")
                 .verifyComplete();
+    }
+
+    @Test
+    public void transformUsingFlatMap() {
+        Flux<String> namesFlux = Flux.fromIterable(Arrays.asList("A", "B", "C", "D", "E", "F")) //A,B,C,D,E,F
+                .flatMap(s -> {
+                    return Flux.fromIterable(convertToList(s)); // A -> List[A, newValue], B-> List[B, newValue]
+                })  //db or external service call that returns a flux -> s -> Flux<String>
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+    }
+
+    @Test
+    public void transformUsingFlatMap_usingParallel() {
+        Flux<String> namesFlux = Flux.fromIterable(Arrays.asList("A", "B", "C", "D", "E", "F")) // Flux<String>
+                .window(2) // Flux<Flux<String> -> (A,B), (C,D), (E,F) | window() uzywane jest do przekazywania wiecej elementow z Flux
+                .flatMap((s) ->
+                        s.map(this::convertToList).subscribeOn(parallel())) // Flux<List<String>  | parallel -> czuwa by rzeczy wykonywaly sie rownolegle
+                .flatMap(s -> Flux.fromIterable(s))   // Flux<String>
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+    }
+
+    @Test
+    public void transformUsingFlatMap_parallel_maintain_order() {
+        Flux<String> namesFlux = Flux.fromIterable(Arrays.asList("A", "B", "C", "D", "E", "F")) // Flux<String>
+                .window(2) // Flux<Flux<String> -> (A,B), (C,D), (E,F) | window() uzywane jest do przekazywania wiecej elementow z Flux
+//                .concatMap((s) ->
+//                        s.map(this::convertToList).subscribeOn(parallel())) // Flux<List<String>  | parallel -> czuwa by rzeczy wykonywaly sie rownolegle
+
+                .flatMapSequential((s) ->
+                        s.map(this::convertToList).subscribeOn(parallel()))
+                .flatMap(s -> Flux.fromIterable(s))   // Flux<String>
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+    }
+
+
+    private List<String> convertToList(String s) {
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Arrays.asList(s, "newValue");
     }
 }
