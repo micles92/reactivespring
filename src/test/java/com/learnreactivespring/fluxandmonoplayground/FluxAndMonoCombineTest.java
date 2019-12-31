2d1b2fab8897612e9871901312c246a34b929a1c
E050AQ <mlesniak@genuitek.com>
2019-12-31 13:13:42 +0100

    VirtualTime - zmiany w testach

--- src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoCombineTest.java
+++ src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoCombineTest.java
@@ -3,6 +3,7 @@ package com.learnreactivespring.fluxandmonoplayground;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
 
 import java.time.Duration;
 
@@ -23,15 +24,23 @@ public class FluxAndMonoCombineTest {
 
     @Test
     public void combineUsingMerge_withDelay() {
+        VirtualTimeScheduler.getOrSet();
+
         Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
         Flux<String> flux2 = Flux.just("D", "E", "F").delayElements(Duration.ofSeconds(1));
 
         Flux<String> mergedFlux = Flux.merge(flux1, flux2);
 
-        StepVerifier.create(mergedFlux.log())
+//        StepVerifier.create(mergedFlux.log())
+//                .expectSubscription()
+//                .expectNextCount(6)
+////                .expectNext("A","B","C","D","E","F")
+//                .verifyComplete();
+
+        StepVerifier.withVirtualTime(() -> mergedFlux.log())
                 .expectSubscription()
+                .thenAwait(Duration.ofSeconds(6))
                 .expectNextCount(6)
-//                .expectNext("A","B","C","D","E","F")
                 .verifyComplete();
     }
 
@@ -51,15 +60,25 @@ public class FluxAndMonoCombineTest {
 
     @Test
     public void combineUsingConcat_withDelay() {
+        VirtualTimeScheduler.getOrSet();
+
         Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
         Flux<String> flux2 = Flux.just("D", "E", "F").delayElements(Duration.ofSeconds(1));
 
         Flux<String> mergedFlux = Flux.concat(flux1, flux2);
 
-        StepVerifier.create(mergedFlux.log())
+//        StepVerifier.create(mergedFlux.log())
+//                .expectSubscription()
+//                .expectNext("A","B","C","D","E","F")
+//                .verifyComplete();
+
+
+        StepVerifier.withVirtualTime(() -> mergedFlux.log())
                 .expectSubscription()
+                .thenAwait(Duration.ofSeconds(6))
                 .expectNext("A","B","C","D","E","F")
                 .verifyComplete();
+
     }
 
     @Test
