dd84ba63d14e843b75e3a3167d541964bbc8b88b
E050AQ <mlesniak@genuitek.com>
2020-01-06 22:23:19 +0100

    getOneItem + test

--- src/test/java/com/learnreactivespring/controller/v1/ItemControllerTest.java
+++ src/test/java/com/learnreactivespring/controller/v1/ItemControllerTest.java
@@ -99,4 +99,20 @@ public class ItemControllerTest {
                 .expectNextCount(4)
                 .verifyComplete();
     }
+
+    @Test
+    public void getOneItem() {
+        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"), "ABC")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.price", 149.99);
+    }
+
+    @Test
+    public void getOneItem_notFound() {
+        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"), "DEF")
+                .exchange()
+                .expectStatus().isNotFound();
+    }
 }
