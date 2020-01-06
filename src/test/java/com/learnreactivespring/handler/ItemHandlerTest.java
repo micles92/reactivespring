ebe824bbb31eec2e439bfad9c55b801ae33ae3c3
E050AQ <mlesniak@genuitek.com>
2020-01-06 23:35:24 +0100

    Functional Web - Rest do get ALlItems

---
+++ src/test/java/com/learnreactivespring/handler/ItemHandlerTest.java
@@ -0,0 +1,104 @@
+package com.learnreactivespring.handler;
+
+import com.learnreactivespring.constants.ItemConstants;
+import com.learnreactivespring.document.Item;
+import com.learnreactivespring.repository.ItemReactiveRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.learnreactivespring.constants.ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1;
+import static org.junit.Assert.assertTrue;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@DirtiesContext
+@AutoConfigureWebTestClient
+@ActiveProfiles("test")
+public class ItemHandlerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    private ItemReactiveRepository itemReactiveRepository;
+
+
+    @Before
+    public void setUp() {
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(data())).flatMap(itemReactiveRepository::save)
+                .doOnNext((item -> System.out.println("Inserted item: is:" + item)))
+                .blockLast();
+    }
+
+
+    public List<Item> data() {
+        return Arrays.asList(new Item(null, "Samsung Tv", 399.9),
+                new Item(null, "LG Tv", 329.9),
+                new Item(null, "Apple Watch", 349.9),
+                new Item("ABC", "Beats HeadPhones", 19.99));
+    }
+
+
+    @Test
+    public void getAllItems() {
+
+        webTestClient.get().uri(ITEM_FUNCTIONAL_END_POINT_V1)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Item.class)
+                .hasSize(4);
+    }
+
+    @Test
+    public void testGetAllItems_approach2() {
+
+        webTestClient.get().uri(ITEM_FUNCTIONAL_END_POINT_V1)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Item.class)
+                .hasSize(4)
+                .consumeWith(response -> {
+                    List<Item> items =  response.getResponseBody();
+                    items.forEach(item1 -> {
+                        assertTrue(item1.getId()!=null);
+                    });
+
+                });
+    }
+
+    @Test
+    public void testGetAllItems_approach3() {
+
+
+        Flux<Item> itemsFlux = webTestClient.get().uri(ITEM_FUNCTIONAL_END_POINT_V1)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .returnResult(Item.class)
+                .getResponseBody();
+
+        StepVerifier.create(itemsFlux)
+                .expectNextCount(4)
+                .verifyComplete();
+    }
+}
