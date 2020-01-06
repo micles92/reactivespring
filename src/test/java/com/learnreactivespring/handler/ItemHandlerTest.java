a432df13cf0e464107cb1050f2dd1738a2a08b3d
E050AQ <mlesniak@genuitek.com>
2020-01-06 23:45:08 +0100

    Functional Web - Rest do GetOneItem()

--- src/test/java/com/learnreactivespring/handler/ItemHandlerTest.java
+++ src/test/java/com/learnreactivespring/handler/ItemHandlerTest.java
@@ -101,4 +101,21 @@ public class ItemHandlerTest {
                 .expectNextCount(4)
                 .verifyComplete();
     }
+
+    @Test
+    public void getOneItem() {
+        webTestClient.get().uri(ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1.concat("/{id}"), "ABC")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.price", 149.99);
+    }
+
+    @Test
+    public void getOneItem_notFound() {
+        webTestClient.get().uri(ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1.concat("/{id}"), "DEF")
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
 }
